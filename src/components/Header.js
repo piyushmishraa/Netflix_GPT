6c6183213bc7dbf7e3c5e73adf712c314adec660
Piyush Mishra <fanboiofharrystyles@gmail.com>
2023-11-24 11:50:19 +0530

    added netflicx logo and background

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='absolute'>
+      <img className='w-44 bg-gradient-to-b from-black ' src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' alt='netflix'/>
+      </div>
   )
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from './Header'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+       <Header/>
+       <img src='https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/67033404-2df8-42e0-a5a0-4c8288b4da2c/IN-en-20231120-popsignuptwoweeks-perspective_alpha_website_medium.jpg' alt='background'/>   
+    </div>
+    
   )
 }
 
