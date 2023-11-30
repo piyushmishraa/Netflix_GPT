bd863a3ad8d190891a2987159b168d0863bab664
Piyush Mishra <fanboiofharrystyles@gmail.com>
2023-11-30 14:09:23 +0530

    added logout functionality

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import {  signOut } from "firebase/auth";
+import { auth } from '../utils/firebase';
+import { useNavigate } from 'react-router-dom';
+
 
 const Browse = () => {
+  const navigate=useNavigate();
+  const handleLogout=()=>{
+    signOut(auth).then(() => {
+      navigate("/");
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
   return (
-    <div>Browse</div>
+    <div>
+      <button className=' border-2 ' onClick={handleLogout}> Log Out test</button>
+    </div>
   )
 }
 
