a75ec27e269669ca17aca4903fcfdae5f80eef75
Piyush Mishra <fanboiofharrystyles@gmail.com>
2023-12-01 09:59:53 +0530

    tested sign out

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,21 +1,33 @@
 import React from 'react'
-import {  signOut } from "firebase/auth";
+
+import Header from './Header';
+import { signOut } from "firebase/auth";
 import { auth } from '../utils/firebase';
 import { useNavigate } from 'react-router-dom';
 
 
+
 const Browse = () => {
   const navigate=useNavigate();
-  const handleLogout=()=>{
+
+  const handleSignOut=()=>{
     signOut(auth).then(() => {
-      navigate("/");
+      // Sign-out successful.
+      navigate('/');
+       
+
     }).catch((error) => {
-      console.log(error);
+      // An error happened.
+      navigate('/error');
     });
+
   }
+  
+ 
   return (
     <div>
-      <button className=' border-2 ' onClick={handleLogout}> Log Out test</button>
+    <Header/>
+    <button className=' border-2 absolute my-20' onClick={handleSignOut} >Test Sign Out </button>
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import {  signOut } from "firebase/auth";
+import { auth } from '../utils/firebase';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+
+
   return (
-    <div className='absolute z-10'>
-      <img className='w-44 bg-gradient-to-b from-black ' src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' alt='netflix'/>
+    <div className='absolute z-10 w-screen bg-gradient-to-b from-black py-2 px-8 flex justify-between'>
+      <img className='w-44  ' src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png ' alt='netflix'/>
+         <div className='flex'>
+          <img className='w-8 h-8 my-6' src='https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg' alt='usericon'/>
+           <button className='mx-2 p-2 text-white font-bold' >Sign Out</button>
+         </div>
       </div>
+      
   )
 }
 
