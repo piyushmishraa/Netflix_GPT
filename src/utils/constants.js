e1301f11282d914d32fec29a1ceaaa286c2d3987
Piyush Mishra <fanboiofharrystyles@gmail.com>
2023-12-05 20:21:39 +0530

    got the tmdb api key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,10 @@
 
  export const userLogo=
  "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg"
+
+ export const API_KEY=
+ "d145efb0c8f5a9b7394937d31ea3178c"
+
+ export const API_TOKEN=
+
+"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMTQ1ZWZiMGM4ZjVhOWI3Mzk0OTM3ZDMxZWEzMTc4YyIsInN1YiI6IjY1NmYzMWU3NTY4NDYzMDEwZjg0OTc0ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AmRPN7T1jV75Xwegfu_V3qZq5IM2zV9Q6rf_pbHSRFo"
