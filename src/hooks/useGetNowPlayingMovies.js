1127515cb79951dc0142ec5aebd6c1b8b1af58b5
Piyush Mishra <fanboiofharrystyles@gmail.com>
2023-12-08 10:22:21 +0530

    created a custome hook for API fetch and adding data to store,Refactored our code

---
+++ src/hooks/useGetNowPlayingMovies.js
@@ -0,0 +1,26 @@
+import { useDispatch } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+import { addNowPlayingMovies } from "../utils/movieSlice";
+import { useEffect } from "react";
+
+
+
+
+const useGetNowPlayingMovies=()=>{
+    const dispatch= useDispatch();
+    const getNowPlayingMovies=async()=>{
+         const data=await fetch("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",API_OPTIONS);
+         const json=await data.json();
+         console.log(json.results);
+         dispatch(addNowPlayingMovies(json.results))
+  
+    }
+  
+    useEffect(()=>{
+     
+         getNowPlayingMovies();
+          // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
+}
+
+export default useGetNowPlayingMovies ;
